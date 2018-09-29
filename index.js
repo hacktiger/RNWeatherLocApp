14cd3f6ecc5971ac8e3244935246dbd51e68d91b
hacktiger <hacktiegr1989@gmail.com>
2018-09-29 21:01:22 +0700

    Styles + header + props

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+// Libaries
+import React from 'react';
 import { AppRegistry } from 'react-native';
-import App from './App';
 
+//My Components
+//import App from './App';
+import Header from './src/components/header'
+
+//Create comp
+const App = () => (
+	<Header headerText = { ' INDEX PAGE ' } />
+);
+
+//Render
 AppRegistry.registerComponent('WorkingApp', () => App);
