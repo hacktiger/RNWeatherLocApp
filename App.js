8d445db97e5d96587451d7e54f9c3d3e3ae19e5e
hacktiger <hacktiegr1989@gmail.com>
2018-09-29 11:06:31 +0700

    geolocation

--- App.js
+++ App.js
@@ -5,10 +5,58 @@
  */
 import React, { Component } from 'react'
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet,PermissionsAndroid ,Text, Button } from 'react-native'
 
 
-export default class MapTest extends Component {
+async function requestGPSPermission() {
+    console.log('Activated func');
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          'title': 'Cool Photo App Camera Permission',
+          'message': 'Cool Photo App needs access to your camera ' +
+                     'so you can take awesome pictures.'
+        }
+      )
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log("You can use the camera");
+      } else {
+        console.log("Camera permission denied");
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+}
+export default class App extends Component {
+
+  
+  render() {
+    return (
+      <View style={styles.container}>
+
+        <Button
+          onPress={() => {
+            console.log('Pressed');
+            requestGPSPermission();
+            navigator.geolocation.getCurrentPosition(
+              (position) => console.log(position),
+              (err) => console.log(err),
+               {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+            );
+
+            //Alert.alert('You tapped the button!');
+          }}
+          title="Display GPS"
+        />
+      </View>
+    );
+  }
+}
+
+
+ class MapTest extends Component {
+
   render () {
     return (
       <View style={styles.container}>
@@ -16,16 +64,16 @@ export default class MapTest extends Component {
           provider={PROVIDER_GOOGLE}
           style={styles.map}
           region={{
-            latitude: 43.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
+            latitude: 21.026416,
+            longitude: 105.851541,
+            latitudeDelta: 0.1,
+            longitudeDelta: 0.1,
           }}
         >
           <MapView.Marker
             coordinate = {{
-              latitude: 43.78825,
-              longitude: -122.4324
+              latitude: 21.026416,
+              longitude: 105.851541,
             }}
             title = {'Title'}
             description = {'description'}
@@ -35,7 +83,13 @@ export default class MapTest extends Component {
     )   
   }
 }
+
 const styles = StyleSheet.create({
   container: { ... StyleSheet.absoluteFillObject },
   map: { ...StyleSheet.absoluteFillObject }
 })
+
+
+/**
+*   STYLESHEET
+*/
