74c8735a4fb537253b8c98655917fd88ad379277
hacktiger <hacktiegr1989@gmail.com>
2018-09-29 09:28:26 +0700

    added

---
+++ App.js
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+import React, { Component } from 'react'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+import { View, StyleSheet } from 'react-native'
+class MapTest extends Component {
+  render () {
+    return (
+      <View style={styles.container}>
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          region={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+        ></MapView>
+      </View>
+    )   
+  }
+}
+const styles = StyleSheet.create({
+  container: { ... StyleSheet.absoluteFillObject },
+  map: { ...StyleSheet.absoluteFillObject }
+})
+export default MapTest
