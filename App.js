7f1656b5f098195743a5a88a306ed2aedd548097
unknown <hacktiegr1989@gmail.com>
2018-11-03 10:02:15 +0700

    deleting stack => conly tab

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import {
-  Button,
-  View,
   StyleSheet,
   Dimensions,
   Text } from 'react-native'
@@ -12,27 +10,9 @@ import WeatherForecastViewer from './src/view/Screens/WeatherForecast/WeatherFor
 import MapViewer from './src/view/Screens/Map/MapViewer'
 import UserViewer from './src/view/Screens/User/UserViewer'
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
-import { createStackNavigator } from 'react-navigation'
+// import { createStackNavigator } from 'react-navigation'
 import Icon from 'react-native-vector-icons/Ionicons'
 // Classes
-//
-class HomeScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Home'
-  }
-  render () {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Go to UserView"
-          // onPress change navigate('x') x: @ref RootStack
-          onPress={() => this.props.navigation.navigate('UserView')}
-        />
-      </View>
-    )
-  }
-}
 // 
 class UserViewScreen extends React.Component {
   static navigationOptions = {
@@ -74,82 +54,45 @@ class SettingsScreen extends React.Component {
     )
   }
 }
-// 
-const HeaderOptions = {
-  headerStyle: {
-    backgroundColor: '#f4511e',
-  },
-  headerTintColor: '#fff',
-  headerTitleStyle: {
-    fontWeight: 'bold',
-  }
-}
-// 
-const HomeStack = createStackNavigator(
-  {
-  User: { screen: UserViewScreen } 
-  },
-  {
-    navigationOptions: HeaderOptions
-  }
-)
-// 
-const WeatherStack = createStackNavigator(
-  {
-    Map: { screen: MapViewScreen }
-  },
-  {
-    // after merging Map and Forecast => initial => delete
-    initialRouteName: 'Map',
-    navigationOptions: HeaderOptions
-  }
-)
-// 
-const SettingsStack = createStackNavigator(
-  {
-  Settings: { screen: SettingsScreen }
-  },
-  {
-    navigationOptions: HeaderOptions
-  }
-)
 
 export default createMaterialBottomTabNavigator(
   {
-    Home: { screen: HomeStack, 
+    // RouteConfig
+    Home: { screen: UserViewScreen, 
       navigationOptions: {
         tabBarLabel: 'Home',
         tabBarIcon: ({ tintColor }) => (
           <Icon name='ios-home' color={tintColor} size={24} />
-        )
+        ),
+        tabBarColor : 'purple'
       }
     },
-    Forecast: { screen: WeatherStack,
+    Forecast: { screen: MapViewScreen,
       navigationOptions: {
         tabBarLabel: 'Map',
         tabBarIcon: ({ tintColor }) => (
           <Icon name='ios-map' color={tintColor} size={24} />
-        )
+        ),
+        tabBarColor: '#f4511e'
       }  
     },
-    Settings: { screen: SettingsStack,
+    Settings: { screen: SettingsScreen,
       navigationOptions: {
         tabBarLabel: 'Settings',
         tabBarIcon: ({ tintColor }) => (
           <Icon name='ios-settings' color={tintColor} size={24} />
-        )
+        ),
+        tabBarColor: 'green'
       } 
     }
-    // Login: { screen: LoginStack },
   },
   {
+    // TabConfig
     initialRouteName: 'Home',
-    activeColor: '#ffffff',
-    inactiveColor: '#b2b2b2',
-    barStyle: { 
-      backgroundColor: '#f4511e',
-    }, 
-    
+    shifting: true,
+    activeColor: 'white',
+    inactiveColor: 'grey',
+    barStyle :{  }
   }
 )
 
@@ -165,7 +108,6 @@ const styles = StyleSheet.create({
   footerContainer: {  
     width: Dimensions.get('window').width,
     flexGrow: 0.1,
-    backgroundColor: '#3b5998',
     alignItems: 'center',
     justifyContent: 'center'
   }
