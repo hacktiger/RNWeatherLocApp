451e64ba921c76b59dc10586f723a4b6376ec707
unknown <hacktiegr1989@gmail.com>
2018-11-07 17:42:44 +0700

    fix main chat function

--- src/controller/Firebase.js
+++ src/controller/Firebase.js
@@ -1,9 +1,30 @@
 import FirebaseDataService from '../services/models/FirebaseDataService'
 
 class Firebase {
-  constructor () {
+  uid = '';
+  messagesRef = null;
+
+  constructor() {
     this.FirebaseSingleton = FirebaseDataService.getInstance()
+    this.onStateChanged()
   }
+
+  onStateChanged = () => {
+    this.FirebaseSingleton.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setUid(user.uid)
+      }
+    })
+  }
+
+  setUid (id) {
+    this.uid = id
+  }
+
+  getUid () {
+    return this.uid
+  }
+
   logInUser (email, password) {
     return this.FirebaseSingleton.auth().signInWithEmailAndPassword(email, password)
   }
@@ -20,6 +41,42 @@ class Firebase {
   database () {
     return this.FirebaseSingleton.database()
   }
+
+  loadMessages (callback) {
+    this.messagesRef = this.database().ref('messages')
+    this.messagesRef.off()
+
+    const onReceive = (data) => {
+      const message = data.val()
+      callback({
+        _id: data.key,
+        text: message.text,
+        createdAt: new Date(message.createdAt),
+        user: {
+          _id: message.user._id,
+          name: message.user.name
+        }
+      })
+    }
+    this.messagesRef.limitToLast(20).on('child_added', onReceive);
+  }
+
+  sendMessage (message) {
+    for(let i = 0; i < message.length; i++) {
+      this.messagesRef.push({
+        text: message[i].text,
+        user: message[i].user,
+        createdAt: '2017-10-24T09:24:03.329Z'
+      })
+    }
+  }
+
+  closeChatConn () {
+    if (this.messagesRef) {
+      this.messagesRef.off()
+    }
+  }
+// END CLASS
 }
 
 export default Firebase
--- src/view/Screens/User/ChatViewer.js
+++ src/view/Screens/User/ChatViewer.js
@@ -1,9 +1,5 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
-import {
-  View,
-  Text
-} from 'react-native'
 import { GiftedChat } from 'react-native-gifted-chat'
 import Firebase from '../../../controller/Firebase'
 
@@ -14,50 +10,21 @@ class ChatViewer extends React.Component {
       messages: []
     }
     this.myFirebase = new Firebase()
-    this.dtb = this.myFirebase.database()
-    this.messagesTable = this.dtb.ref('message')
-    this.loadMessages()
   }
-  // Retrieve the messages
-  loadMessages () {
-    //
-    if (this.state.loaded) {
-    } else {
-      this.messagesTable.on('value', (snap) => {
-        console.log(snap.val())
-        this.addMessage(snap.val())
-      }, (errObj) => {
-        console.log('read failed :', errObj.code)
+  // lief cycle methods
+  componentDidMount () {
+    
+    this.myFirebase.loadMessages((message) => {
+      console.log(message)
+      this.setState((previousState) => {
+        return {
+          messages: GiftedChat.append(previousState.messages, message)
+        }
       })
-    }
-  }
-
-  addMessage (messages = []) {
-    var keys = Object.keys(messages)
-    for (var i = 0; i < keys.length; i++) {
-      var key = keys[i]
-      var mess = messages[key]
-      this.setState({ messages: [mess, ...this.state.messages] })
-    }
-  }
-
-  _onSend (messages = []) {
-    let messObj = {
-      _id: messages[0]._id,
-      text: messages[0].text,
-      createdAt: '2017-10-24T09:24:03.329Z',
-      user: {
-        _id: messages[0].user._id,
-        name: 'React Native',
-        avatar: 'https://facebook.github.io/react/img/logo_og.png'
-      }
-    }
-
-    this.messagesTable.push(messObj, (err) => {
-      if (err) {
-        console.log('opps')
-      }
     })
+  }
+  componentWillUnmount () {
+    this.myFirebase.closeChatConn()
   }
 
   // MAIN RENDER
@@ -65,9 +32,9 @@ class ChatViewer extends React.Component {
     return (
       <GiftedChat
         messages={this.state.messages}
-        onSend={messages => this._onSend(messages)}
+        onSend={messages => this.myFirebase.sendMessage(messages)}
         user={{
-          _id: 1 // need to be changed to current user ID
+          _id: this.myFirebase.getUid()// need to be changed to current user ID
         }}
       />
     )
